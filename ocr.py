ba895c9c05ce18e929f1e6397d4b34f0f92e5054
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-05-23 13:53:31 -0400

    API request sucess
    API request successful, Returns correct list of TextAnnotationObjects

--- ocr.py
+++ ocr.py
@@ -1,4 +1,5 @@
 import requests 
+import json 
 from google.oauth2 import service_account
 from google.cloud import vision
 import os #Allows interaction with OS 
@@ -8,12 +9,10 @@ import io #Input/output operations
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/anamuuenishi/Desktop/dataEntryEnv/dataentryautomation-793ea24c158f.json'
 client = vision.ImageAnnotatorClient() #Creating cloud object to interact with GoogleVisionAPI
 
-path = '/Users/anamuuenishi/Desktop/dataEntryEnv/0001.jpg'
+path = '/Users/anamuuenishi/Desktop/dataEntryEnv/0002.jpg'
 with io.open(path, 'rb') as image_file: #read in binary mode 
     binaryImg = image_file.read()
-image = vision.Image(content=binaryImg) #Creating an image object 
-response = vision.text_detection()
-
-
-
+clientImage = vision.Image(content=binaryImg) #Creating an image object 
+response = client.text_detection(image=clientImage)
+texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
 
