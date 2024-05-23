0abf2d7dc34d1db416afe5f601ce05c4c8465aa7
mtchoumi <tchoumi31mo@gmail.com>
2024-05-23 13:45:45 -0700

    more commit

--- ocr.py
+++ ocr.py
@@ -6,13 +6,14 @@ import os #Allows interaction with OS
 import io #Input/output operations 
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/anamuuenishi/Desktop/dataEntryEnv/dataentryautomation-793ea24c158f.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './dataentryautomation-793ea24c158f.json'
 client = vision.ImageAnnotatorClient() #Creating cloud object to interact with GoogleVisionAPI
 
-path = '/Users/anamuuenishi/Desktop/dataEntryEnv/0002.jpg'
+path = './0001.jpg'
 with io.open(path, 'rb') as image_file: #read in binary mode 
     binaryImg = image_file.read()
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
+print(texts)
 
