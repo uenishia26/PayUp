aaffd409e4b494aa381d5c79d4b7fcae6c1e3767
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-05-25 13:12:37 -0400

    Temp Fix Image Rotation
    Image rotation temporary fix
    Currently we are assuming all EXIF IPhone photos are captured as a 90 CW
    Function will rotate image 90 then send image to OCR API

--- ocr.py
+++ ocr.py
@@ -4,16 +4,41 @@ from google.oauth2 import service_account
 from google.cloud import vision
 import os #Allows interaction with OS 
 import io #Input/output operations 
+from PIL import Image
+
+def removeExif(imgPath):
+    image = Image.open(imgPath)
+    data = list(image.getdata())
+    imageWithoutExif = Image.new(image.mode, image.size)
+    imageWithoutExif.putdata(data)
+    imageWithoutExif.save(imgPath)
+
+#Temprorary fix 
+def rotateImage(imgPath):
+    
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './dataentryautomation-793ea24c158f.json'
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/anamuuenishi/Desktop/dataEntryEnv/dataentryautomation-793ea24c158f.json'
 client = vision.ImageAnnotatorClient() #Creating cloud object to interact with GoogleVisionAPI
 
-path = './0001.jpg'
+
+path = '/Users/anamuuenishi/Desktop/dataEntryEnv/IMG003.jpg'
+removeExif(path)
+
 with io.open(path, 'rb') as image_file: #read in binary mode 
     binaryImg = image_file.read()
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
-print(texts)
+
+f = open('/Users/anamuuenishi/Desktop/dataEntryEnv/data.txt', 'a')
+
+for text in texts: 
+    f.write(f"Description: {text.description}\n")
+    f.write("Vertices:\n")
+    for vertex in text.bounding_poly.vertices: 
+        f.write(f"({vertex.x}, {vertex.y})\n")
+    f.write("\n")
+
 
