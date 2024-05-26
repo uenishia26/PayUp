42ab621d12d6c0b4aba7199fa65ca85ff709ff7f
mtchoumi <tchoumi31mo@gmail.com>
2024-05-26 15:40:41 -0700

    merge

--- ocr.py
+++ ocr.py
@@ -9,11 +9,11 @@ import io #Input/output operations
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './dataentryautomation-793ea24c158f.json'
 client = vision.ImageAnnotatorClient() #Creating cloud object to interact with GoogleVisionAPI
 
-path = './0001.jpg'
+path = './output/image_6209779.jpg'
 with io.open(path, 'rb') as image_file: #read in binary mode 
     binaryImg = image_file.read()
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
-print(texts)
-
+print(texts[0])
+print(texts[1])
