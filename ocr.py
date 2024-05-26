30b65ee69e71487a275b2cacaf92362dc8368cd7
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-05-26 18:39:04 -0400

    API taking to long

--- ocr.py
+++ ocr.py
@@ -8,18 +8,29 @@ from PIL import Image
 
 #Function to remove exif
 def removeExif(imgPath):
-    image = Image.open(imgPath)
-    data = list(image.getdata())
-    imageWithoutExif = Image.new(image.mode, image.size)
-    imageWithoutExif.putdata(data)
-    imageWithoutExif.save(imgPath)
+    with Image.open(imgPath) as image: 
+        data = list(image.getdata())
+        imageWithoutExif = Image.new(image.mode, image.size)
+        imageWithoutExif.putdata(data)
+        imageWithoutExif.save(imgPath)
+
+def findMidPoint(vertexLst):
+    sumX = None
+    sumY = None
+    for vertex in vertexLst:
+        sumX += vertex.x
+        sumY += vertex.y
+    midX = sumX / vertexLst
+    midY = sumY / vertexLst
+    return (midX, midY)
+    
 
 #Temprorary fix (Rotating Image)
 def rotateImage(imgPath):
     with Image.open(imgPath) as img: #This will automatically close the img after opening
-        rotated_img = img.rotate(90, expand=True) #CW .. expand adjusts frame of photo
-        removeExif(path)
+        rotated_img = img.rotate(-90, expand=True) #CW .. expand adjusts frame of photo
         rotated_img.save(imgPath)
+        removeExif(path)
 
 
 #Creating cloud object to interact with GoogleVisionAPI (Setup for API interaction)
@@ -27,14 +38,15 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/anamuuenishi/Desktop/data
 client = vision.ImageAnnotatorClient() 
 
 #Editting/Sending img to GoogleVision OCR API
-path = '/Users/anamuuenishi/Desktop/dataEntryEnv/Test3.jpg'
-rotateImage(path)
+path = '/Users/anamuuenishi/Desktop/dataEntryEnv/Te2.jpg'
+
 
 with io.open(path, 'rb') as image_file: #read in binary mode 
     binaryImg = image_file.read()
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
+
 
 
 
@@ -47,4 +59,12 @@ for text in texts:
         f.write(f"({vertex.x}, {vertex.y})\n")
     f.write("\n")
 
+#Creating midpoint dictonaries
+text_midpoints = {}
+for text in texts: 
+    key = text.description
+    val = findMidPoint(text.bounding_poly.vertices)
+    text_midpoints[key] = val
+
+print(text_midpoints)
 
