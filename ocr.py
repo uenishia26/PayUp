e8034368a8ab4ba77f15397975012142b2ff41bf
mtchoumi <tchoumi31mo@gmail.com>
2024-06-01 08:48:29 -0700

    clean up ocr file

--- ocr.py
+++ ocr.py
@@ -15,21 +15,18 @@ pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesse
 
 def crop_image(image_path, text):
     image = cv2.imread(image_path)
-    minx = 999999
-    maxx = 0
-    miny = 999999
-    maxy = 0
-    for vertex in text.bounding_poly.vertices:  
-        minx = min(vertex.x, minx)
-        maxx = max(vertex.x, maxx)
-        miny = min(vertex.y, miny)
-        maxy = max(vertex.y, maxy)
-    y= image.shape[1] - maxy
-    x= image.shape[0] - maxx
-    h= image.shape[1] - miny
-    w= image.shape[0] - minx
-    crop_image = image[x:w, y:h]
-    return crop_image
+    if (image.shape[0] > 1000 and image.shape[1] > 1000):
+        xval = []
+        yval = []
+        for vertex in text.bounding_poly.vertices:  
+            xval.append(vertex.x)
+            yval.append(vertex.y)
+        y= image.shape[1] - max(yval)
+        x= image.shape[0] - max(xval)
+        h= image.shape[1] - min(yval)
+        w= image.shape[0] - min(xval)
+        image = image[x:w, y:h]
+    return image
 
 def preprocess_image(image_path, text):
     image = crop_image(image_path, text)
@@ -91,7 +88,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './dataentryautomation-793ea24c15
 client = vision.ImageAnnotatorClient() 
 
 #Editting/Sending img to GoogleVision OCR API
-path = './input/IMG_0210.jpg'
+path = './input/IMG_0220.jpg'
 
 
 with io.open(path, 'rb') as image_file: #read in binary mode 
@@ -99,80 +96,6 @@ with io.open(path, 'rb') as image_file: #read in binary mode
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
-#Function to remove exif
-def removeExif(imgPath):
-    image = Image.open(imgPath)
-    data = list(image.getdata())
-    imageWithoutExif = Image.new(image.mode, image.size)
-    imageWithoutExif.putdata(data)
-    imageWithoutExif.save(imgPath)
-
-#Temprorary fix 
-#Temprorary fix (Rotating Image)
-def rotateImage(imgPath):
-    with Image.open(imgPath) as img: #This will automatically close the img after opening
-        rotated_img = img.rotate(90, expand=True) #CW .. expand adjusts frame of photo
-        removeExif(path)
-        rotated_img.save(imgPath)
-
-
-#Midpoint Function 
-def findMidPoint(vertexLst):
-    sumX = 0
-    sumY = 0
-    for vertex in vertexLst:
-        sumX = sumX + vertex.x
-        sumY = sumY + vertex.y
-    midX = sumX / len(vertexLst)
-    midY = sumY / len(vertexLst)
-    return (midX, midY)
-
-def getOCRdata(imgPath):
-    with io.open(imgPath, 'rb') as image_file: #read in binary mode 
-        binaryImg = image_file.read()
-
-    clientImage = vision.Image(content=binaryImg) #Creating an image object 
-    response = client.text_detection(image=clientImage)
-    texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
-
-    #Creating txt file for parsed OCR Data
-    f = open('/Users/anamuuenishi/Desktop/dataEntryEnv/data.txt', 'a')
-    ocrData = []
-    for text in texts: 
-        vertices = text.bounding_poly.vertices
-        ocrData.append = [{'item':text.description, 
-                           'x1': vertices[0].x, 'y1': vertices[0].y,
-                           'x2': vertices[1].x, 'y2':vertices[1].y,
-                           'x3':vertices[2].x, 'y3':vertices[2].y, 
-                           'x4':vertices[3].x, 'y4':vertices[3].y}]
-    return ocrData 
-
-def normalizeData(ocrData, width, height):
-    for eachData in ocrData: 
-        eachData['x1'] /= width
-        eachData['x2'] /= width 
-        eachData['x3'] /= width 
-        eachData['x4'] /= width
-        eachData['y1'] /= height
-        eachData['y2'] /= height
-        eachData['y3'] /= height
-        eachData['y4'] /= height
-
-    return ocrData
-
-
-
-
-
-#Creating cloud object to interact with GoogleVisionAPI (Setup for API interaction)
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/anamuuenishi/Desktop/dataEntryEnv/dataentryautomation-793ea24c158f.json'
-client = vision.ImageAnnotatorClient() #Creating cloud object to interact with GoogleVisionAPI
-
-
-
-#Editting/Sending img to GoogleVision OCR API
-path = '/Users/anamuuenishi/Desktop/dataEntryEnv/MLModelTrainImages/Test1.jpg'
-ocrData = getOCRdata(path)
 
 try:
     image = preprocess_image(path, texts[0])
@@ -198,7 +121,7 @@ try:
         elif angle == 270 and confidence > 2.0:
             rotated_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
         else:
-            if angle != 0:
+            if (confidence < 2.0):
                 print("Take a better photo")
             else:
                 print("Image is at the correct angle")
@@ -209,7 +132,7 @@ try:
     print(f"Rotated image saved at {output_path}")
     
 except Exception as e:
-    print("Take a better photo")
+    print(f"Take a better photo {e}")
 
 
 #Creating txt file for parsed OCR Data
@@ -220,24 +143,3 @@ for text in texts:
     for vertex in text.bounding_poly.vertices: 
         f.write(f"({vertex.x}, {vertex.y})\n")
     f.write("\n")
-
-# #Creating midpoint dictonaries
-# text_midpoints = {}
-# for text in texts: 
-#     key = text.description
-#     val = findMidPoint(text.bounding_poly.vertices)
-#     text_midpoints[key] = val
-
-# print(text_midpoints)
-
-
-'''
-#Finding Midpoints 
-text_midpoints = {}
-for text in texts: 
-    key = text.description
-    val = findMidPoint(text.bounding_poly.vertices)
-    text_midpoints[key] = val
-
-print(text_midpoints)
-'''
