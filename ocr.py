21cc9817093fd212342e01a5f7432f280d2ef6d1
mtchoumi <tchoumi31mo@gmail.com>
2024-06-07 10:03:11 -0700

    Moryan improvements

--- ocr.py
+++ ocr.py
@@ -27,6 +27,12 @@ def crop_image(image, text):
         image = image[x:w, y:h]
     return image
 
+def ouput_image(image):
+    # Optionally, save or display the rotated image
+    output_path = './output/' + name
+    cv2.imwrite(output_path, image)
+    print(f"Rotated image saved at {output_path}")
+
 def preprocess_image(image_path, text):
     image = cv2.imread(image_path)
     if image is None:
@@ -35,19 +41,58 @@ def preprocess_image(image_path, text):
     # Convert to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+
     # Apply dilation and erosion to remove noise
     kernel = np.ones((1, 1), np.uint8)
     image = cv2.dilate(image, kernel, iterations=1)
     image = cv2.erode(image, kernel, iterations=1)
 
-     # Apply thresholding
-    image = cv2.threshold(cv2.medianBlur(image, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # Apply thresholding
+    thresh = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)[0]
     
     image = crop_image(image, text)
     # Resize the image to improve OCR accuracy
-    image = cv2.resize(image, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    thresh2 = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)[0]
+
+    image = cv2.threshold(image, max(thresh, thresh2), 255, cv2.THRESH_BINARY)[1]
 
     return image
+
+def image_orient(path, texts):
+    try:
+        image = preprocess_image(path, texts[0])
+        
+        start_time = time.time()
+        imginfo = pytesseract.image_to_osd(image, config='--psm 0')
+        elapsed_time = time.time() - start_time
+        print(f"OCR processing time: {elapsed_time:.2f} seconds")
+        
+        angle = extract_info(imginfo, r'Orientation in degrees: \d+', "Angle in degrees")
+        confidence = extract_info(imginfo, r'Orientation confidence: \d+\.\d+', "Confidence in degrees")
+        if confidence:
+            confidence = float(confidence)
+        
+        rotated_image = image  # Default to the original image
+        
+        if angle and confidence:
+            angle = int(angle)
+            if angle == 90 and confidence > 2.0:
+                rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                ouput_image(rotated_image)
+            elif angle == 180 and confidence > 2.0:
+                rotated_image = cv2.rotate(image, cv2.ROTATE_180)
+                ouput_image(rotated_image)
+            elif angle == 270 and confidence > 2.0:
+                rotated_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+                ouput_image(rotated_image)
+            else:
+                if (confidence < 2.0):
+                    print("Take a better photo")
+                else:
+                    ouput_image(rotated_image)
+        
+    except Exception as e:
+        print(f"Take a better photo {e}")
 
 def extract_info(imginfo, pattern, label):
     match = re.search(pattern, imginfo)
@@ -100,44 +145,7 @@ with io.open(path, 'rb') as image_file: #read in binary mode
 clientImage = vision.Image(content=binaryImg) #Creating an image object 
 response = client.text_detection(image=clientImage)
 texts = response.text_annotations #Returns a strcutured return TextAnnotations object 
-
-try:
-    image = preprocess_image(path, texts[0])
-    
-    start_time = time.time()
-    imginfo = pytesseract.image_to_osd(image, config='--psm 0')
-    elapsed_time = time.time() - start_time
-    print(f"OCR processing time: {elapsed_time:.2f} seconds")
-    
-    angle = extract_info(imginfo, r'Orientation in degrees: \d+', "Angle in degrees")
-    confidence = extract_info(imginfo, r'Orientation confidence: \d+\.\d+', "Confidence in degrees")
-    if confidence:
-        confidence = float(confidence)
-    
-    rotated_image = image  # Default to the original image
-    
-    if angle and confidence:
-        angle = int(angle)
-        if angle == 90 and confidence > 2.0:
-            rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        elif angle == 180 and confidence > 2.0:
-            rotated_image = cv2.rotate(image, cv2.ROTATE_180)
-        elif angle == 270 and confidence > 2.0:
-            rotated_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        else:
-            if (confidence < 2.0):
-                print("Take a better photo")
-            else:
-                print("Image is at the correct angle")
-    
-    # Optionally, save or display the rotated image
-    output_path = './output/' + name
-    cv2.imwrite(output_path, rotated_image)
-    print(f"Rotated image saved at {output_path}")
-    
-except Exception as e:
-    print(f"Take a better photo {e}")
-
+image_orient(path,texts)
 
 #Creating txt file for parsed OCR Data
 f = open('./data.txt', 'a')
